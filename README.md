a289cb17bb572d8983fc048772383a5f29c3c515
AbdoArafh <abdoarafh54@gmail.com>
2021-05-26 21:45:12 +0200

    uploading everthing and updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-"# qr-code-generator" 
+# QR Code Generator
+
+This is a simple web aplication made using
+JavaScript, HTML and Bootstrap to generate
+QR codes on the fly choosing from a variaty
+of formats and sizes using [this](goar.me/api/)
+amazing API.
+
+> Hosted on GitHub pages [here](https://abdoarafh.github.io/qr-code-generator/)
---
+++ main.js
@@ -0,0 +1,40 @@
+let urlInput = document.getElementById("url");
+let sizeInput = document.getElementById("size");
+let formatInput = document.getElementById("format");
+let submitButton = document.getElementById("submit");
+let imgContainer = document.getElementById("imgContainer");
+
+for (let i = 1; i <= 20; i++) {
+  option = document.createElement("OPTION");
+  let rez = i * 20;
+  option.value = rez;
+  option.innerHTML = rez + "x" + rez;
+  sizeInput.appendChild(option);
+}
+
+sizeInput.value = 120;
+
+request = () => {
+    if (urlInput.value == "") return;
+  submitButton.disabled = true;
+  submitButton.value = "Loading...";
+  url = "http://api.qrserver.com/v1/create-qr-code/?data=";
+  url += urlInput.value;
+  url += "&size=" + sizeInput.value + "x" + sizeInput.value;
+  url += "&format=" + formatInput.value;
+  fetch(url)
+    .then((response) => response.blob())
+    .then((image) => {
+      img = document.createElement("img");
+      img.src = URL.createObjectURL(image);
+      while (imgContainer.hasChildNodes())
+        imgContainer.removeChild(imgContainer.firstChild);
+      imgContainer.appendChild(img);
+    })
+    .catch((e) => console.log(e));
+  submitButton.disabled = false;
+  submitButton.value = "Generate";
+  urlInput.value = "";
+};
+
+submitButton.addEventListener("click", request);
